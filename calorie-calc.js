f2521f8487951a8ea0bbdc49997221d28eb048c1
Aidan Katz <aidankatz1835@gmail.com>
2022-08-31 14:21:24 -0400

    added units selection for height and weight, sex selection, and BMR calculation for men

--- calorie-calc.js
+++ calorie-calc.js
@@ -1,10 +1,15 @@
+var bw, h, age, lose;
+
 function takeInput() {
-    var bw = document.getElementById("bodyweight").value;
-    var h = document.getElementById("height").value;
-    var age = document.getElementById("age").value;
-    var lose = document.getElementById("lose").value;
-    var gain = document.getElementById("gain").value;
-    var lose;
+    bw = document.getElementById("bodyweight").value;
+    h = document.getElementById("height").value;
+    age = document.getElementById("age").value;
     if (document.getElementById("lose").checked) lose = true;
     else lose = false;
+    calcCalories();
+}
+
+function calcCalories() {
+    //TDEE Calculation (Harris-Benedict Formula)
+    var bmr = 66 + (13.7 * bw) + (5 * h) - (6.8 * age); //basal metabolic rate
 }
