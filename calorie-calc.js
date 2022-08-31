6540bbc586c51acd0a6cb57cc4738317738147cd
Aidan Katz <aidankatz1835@gmail.com>
2022-08-31 13:36:46 -0400

    added script for taking user input

---
+++ calorie-calc.js
@@ -0,0 +1,10 @@
+function takeInput() {
+    var bw = document.getElementById("bodyweight").value;
+    var h = document.getElementById("height").value;
+    var age = document.getElementById("age").value;
+    var lose = document.getElementById("lose").value;
+    var gain = document.getElementById("gain").value;
+    var lose;
+    if (document.getElementById("lose").checked) lose = true;
+    else lose = false;
+}
